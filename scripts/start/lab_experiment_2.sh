a96f799024fa0633da3cd0dc313a0da0d3df01b4
Maik Basso <maik@maikbasso.com.br>
2021-02-11 08:41:59 -0300

    Adding laboratory experiment script, launch and world files.

---
+++ scripts/start/lab_experiment_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# init workspace
+source ~/drone_simulator_ws/src/multi_uav_se_mission/scripts/init/init_ws.sh
+
+# create a results folder if it not exists
+mkdir -p /home/$USER/drone_simulator_ws/src/multi_uav_se_mission/bag
+
+# launch
+roslaunch multi_uav_se_mission lab_experiment_2.launch
